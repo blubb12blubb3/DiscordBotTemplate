c400b3929e0264a3adbbc8716fc1dcf96f9b2121
blubb12blubb3 <larsknittler@gmail.com>
2023-08-31 18:20:17 +0200

    separated msg and msg_id

--- cogs/poll.py
+++ cogs/poll.py
@@ -13,18 +13,21 @@ class Poll(commands.Cog):
         #check for a link and extract the message id
         if "discord" in msg:
             message_link = msg
-            # Split the URL by slashes
+            # split the message link by slashes
             segments = msg.split("/")
-            # Get the last segment (which is the string of numbers you want)
-            msg = segments[-1]
+            # get the last segment (which in this case is the message id)
+            msg_id = segments[-1]
         else:
+            msg_id = msg
             message_link = (f"https://discord.com/channels/{interaction.guild_id}/{interaction.channel_id}/{msg}")
 
+        #adding reactions
         try:
-            message = await self.bot.get_channel(interaction.channel_id).fetch_message(int(msg))
-            await message.add_reaction(':white_check_mark:')
-            await message.add_reaction(':negative_squared_cross_mark:')
-            #add more reactions here if you want)
+            message = await self.bot.get_channel(interaction.channel_id).fetch_message(int(msg_id))
+            await message.add_reaction('✅')
+            await message.add_reaction('❎')
+            #add more reactions here if you want 
+            #for custom emojis write :emoji_name:emoji_id and for animated ones a:emoji_name:emoji_id
             await interaction.response.send_message(f"Command executed on message: {message_link}", ephemeral=True)
         except:
             await interaction.response.send_message(f"Command failed on message: {message_link}", ephemeral=True)
